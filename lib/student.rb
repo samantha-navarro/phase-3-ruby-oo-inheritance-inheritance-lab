2db7b3ea3474361a016b47b4255f341b33d58db8
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-06-29 13:22:01 -0700

    commit tree c0ce8b84131ddae28eee7c08a112064bcff9b2db

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
 
+    attr_reader :knowledge
 end
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+        knowledge
+    end
